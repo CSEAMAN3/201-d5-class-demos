d675c8975d17b2caf119a22a60cf73a4eb9556c9
Chris Seaman <chris@TechEd-0014.techeducators.co.uk>
2023-06-15 15:17:37 +0100

    update

---
+++ 201-class-04/seven.js
@@ -0,0 +1,31 @@
+let locations = ["norwich", "cambridge", "liverpool"];
+
+for (let i = 5; i >= 0; i--) {
+  let winner = false;
+  console.log(winner);
+
+  let attempts = i + 1;
+
+  let city = prompt(
+    "Name one the locations Tech Educators currently offer a coding bootcamp? You have " + attempts + " attempts"
+  ).toLowerCase();
+
+  for (let j = 0; j < locations.length; j++) {
+    if (locations[j] === city) {
+      // console.log("You win");
+      alert("Well done " + locations[j] + " is a correct answer!");
+      winner = !winner;
+      console.log(winner);
+      break;
+    }
+  }
+
+  if (!winner) {
+    alert("Unfortunately " + city + " is not a correct answer!");
+  }
+
+  if (i === 0 || winner) {
+    alert("Tech Ed currently offer bootcamps in Norwich, Cambridge and Liverpool");
+    break;
+  }
+}
